ba64324451901f0d26bfa276c48de898d8116199
Niko Gham <infoscouts@protonmail.com>
2020-08-13 20:48:32 +0100

    EDIT BASIC INFO

--- src/yourdata.js
+++ src/yourdata.js
@@ -4,25 +4,25 @@ export default
         //Change The Website Template
         name :'Full Fat Grappling.',
         headerTagline: [//Line 1 For Header
-                        'Hi 👋 You found me! I am Your Name.',
+                        'Matthew Hucker',
                         //Line 2 For Header
-                        'Digital designer ',
+                        'JUDO ',
                         //Line 3 For Header
-                        'from India'
+                        'BJJ'
     ],
         //Contact Email
-        contactEmail:'hello@chetanverma.com',
+        contactEmail:'hello@fullfatgrappling.com',
         // Add Your About Text Here
-        abouttext: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        aboutImage:'https://images.unsplash.com/photo-1521587765099-8835e7201186?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        abouttext: "Full Fat Martial Arts",
+        aboutImage:'./assets/mainImg.jpg',
        //Change This To Hide The Image of About Section (True Or False)
        ShowAboutImage:true,//true or false (Change Here)
        // Change Projects Here 
        projects:[
            {
             id: 1,//DO NOT CHANGE THIS (Please)😅
-            title:'Project One', //Project Title - Add Your Project Title Here
-             service:'UI/UX Design', // Add Your Service Type Here
+            title:'JUDO', //Project Title - Add Your Project Title Here
+             service:'Close Combat', // Add Your Service Type Here
             //Project Image - Add Your Project Image Here
              imageSrc:"https://images.unsplash.com/photo-1558104631-0fa41a8f6c20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
              //Project URL - Add Your Project Url Here
@@ -33,7 +33,7 @@ export default
                 title: 'Project Two',
                 service: 'Website Development',
                 imageSrc: "https://images.unsplash.com/photo-1554866585-cd94860890b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-                url: 'http://chetanverma.com/'
+                url: 'http://chetanverma.comt4/'
             },
             { 
                 id: 3,//DO NOT CHANGE THIS (Please)😅
@@ -64,19 +64,18 @@ export default
         ],
         social: [
             // Add Or Remove The Link Accordingly
-            {   name:'Github',
-                url:'https://github.com/chetanverma16'},
-            {
-                name: 'Behance',
-                url: 'https://www.behance.net/chetanverma'
-            },
-            {
-                name: 'Dribbble',
-                url: 'https://dribbble.com/chetanverma'
-            },
+            
             {
                 name: 'Instagram',
-                url: 'https://www.instagram.com/cv.uidesign/'
+                url: 'https://www.instagram.com/fullfatjudo/'
+            }, 
+            {
+                name: 'Instagram - Matthew Hucker',
+                url: 'https://www.instagram.com/matthewhucker/'
+            }, 
+            {
+                name: 'Created by Nick @ Small Dog Designs',
+                url: 'https://www.smalldogdesigns.com/'
             }
 
         ]
