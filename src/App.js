41c8675f80b8aa734577a0ef2dd84615056b8f45
Niko Gham <ng@nickgillham.co.uk>
2020-09-06 16:04:23 +0100

    Functionality build

--- src/App.js
+++ src/App.js
@@ -10,8 +10,19 @@ import Navbar from './components/Navbar'
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <div className="App-header">
       <Header></Header>
+      </div>
+      
+      <div className='About'>
+        <About></About>
+      </div>
+      <div className='work'>
+        <Work></Work>
+      </div>
+      <div className='contact'>
+        <Contact></Contact>
       </div>
     </div>
   );
