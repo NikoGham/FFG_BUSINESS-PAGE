45e83daad56eb178720ba9c3a3a0c3e3cccf60cf
Niko Gham <ng@nickgillham.co.uk>
2020-09-09 13:56:03 +0100

    BURGER MENU ADD

--- src/index.js
+++ src/index.js
@@ -12,8 +12,27 @@ import Contact from './components/Contact';
 ReactDOM.render(<Header />, document.getElementById('header'));
 /*ReactDOM.render(<About />, document.getElementById('about'));
 ReactDOM.render(<Work />, document.getElementById('work'));
-ReactDOM.render(<Contact />, document.getElementById('contact'));
+ReactDOM.render(<Contact />, document.getElementById('contact')); 
 
 
 /*  NIKO WHAT IF YOU IMPORT THE INDIVIDUAL COMPONENTS AND THEN 
 RENDER THEM SEPERATELY, THEN MAKE UP A BIT OF CONDITIONAL JS DEPENDENT ON A CLICK EVENT?*/
+
+
+const menuBtn = document.querySelector('.menu-btn');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+     // Set Menu State
+     showMenu = true;
+    } else {
+      menuBtn.classList.remove('close');
+        // Set Menu State
+    showMenu = false;
+}
+}
