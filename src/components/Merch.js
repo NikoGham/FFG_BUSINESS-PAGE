2c3d24c062b80f0046548e88e89077c954e041bb
Niko Gham <ng@nickgillham.co.uk>
2020-09-13 18:03:20 +0100

    Add New Pages

---
+++ src/components/Merch.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class Merch extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Merch</h1>
+            </div>
+        )
+    }
+}
+
+export default Merch
--- src/index.js
+++ src/index.js
@@ -6,17 +6,24 @@ import Header from './components/Header';
 import About from './components/About';
 import Work from './components/work';
 import Contact from './components/Contact';
-
+import Adult from './components/Adult';
+import Membership from './components/Membership';
+import Timetable from './components/Timetable';
+import Merch from './components/Merch';
 
 ReactDOM.render(<Header />, document.getElementById('header'));
 ReactDOM.render(<About />, document.getElementById('about'));
 ReactDOM.render(<Work />, document.getElementById('work'));
-ReactDOM.render(<Contact />, document.getElementById('contact')); 
-
+ReactDOM.render(<Contact />, document.getElementById('contact'));
+ReactDOM.render(<Adult />, document.getElementById('adult'));
+ReactDOM.render(<Membership />, document.getElementById('membership')); 
+ReactDOM.render(<Timetable />, document.getElementById('timetable')); 
+ReactDOM.render(<Merch />, document.getElementById('merch')); 
 
 /*  NIKO WHAT IF YOU IMPORT THE INDIVIDUAL COMPONENTS AND THEN 
-RENDER THEM SEPERATELY, THEN MAKE UP A BIT OF CONDITIONAL JS DEPENDENT ON A CLICK EVENT?*/
+RENDER THEM SEPERATELY, THEN MAKE UP A BIT OF CONDITIONAL JS DEPENDENT ON A CLICK EVENT?
 
+Well this seems to work... must be a cleaner way to do it though ; 13.09.20 */
 
 const menuBtn = document.querySelector('.menu-btn');
 
@@ -27,11 +34,11 @@ menuBtn.addEventListener('click', toggleMenu);
 function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.add('close');
-     // Set Menu State
-     showMenu = true;
-    } else {
-      menuBtn.classList.remove('close');
-        // Set Menu State
+    // Set Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    // Set Menu State
     showMenu = false;
-}
+  }
 }
