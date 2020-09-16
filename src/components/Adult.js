a526fbad711636e3b8910fb7248580e1d60de38b
Niko Gham <ng@nickgillham.co.uk>
2020-09-16 14:01:53 +0100

    install Reactstrap and fix header

--- src/components/Adult.js
+++ src/components/Adult.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Container, Row, Col, Jumbotron } from 'reactstrap';
+import { Button } from 'reactstrap';
 
-class Adult extends Component {
-  render() {
-    return (
-      <div>
-        <h1> hello!</h1>
-      </div>
-    );
-  }
-}
+/* Remember Niko, it's just fancy Javascript */ 
+
+
+const Adult = (props) => {
+
+  
+  return (
+    <Container>
+      <Row>
+        <Col>
+        <Jumbotron >
+        <h1 className="display-3">Adult Athletes</h1>
+        <p className="lead">This is a simplze hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr className="my-2" />
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <p className="lead">
+          <Button color="primary">Learn More</Button>
+        </p>
+        </Jumbotron>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+
 
 export default Adult;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,23 +3,24 @@ import Fade from 'react-reveal/Fade';
 import Flash from 'react-reveal/Flash';
 import Pulse from 'react-reveal/Pulse';
 import data from '../yourdata';
+import { Container, Row, Col, Jumbotron } from 'reactstrap';
 
 class Header extends Component {
   state = {};
 
   render() {
+  
+
     return (
-      <div>
-        <header></header>
-        <Flash forever={true} duration={3000} cascade>
-          {' '}
-          <h1 className="heading-background">FULLFAT</h1>
-        </Flash>
+      <Container>
+        <Row>
+          <Col>
+       
         <p className="header-title">
           <Fade duration={5000} delay={2000}>
             <span className="nameHeader">{data.headerTagline[0]}</span>
           </Fade>
-
+         
           <span className="caramelFont">
             <Pulse> {data.headerTagline[1]}</Pulse>
 
@@ -29,31 +30,18 @@ class Header extends Component {
 
             <Pulse delay={1750}>{data.headerTagline[4]}</Pulse>
           </span>
+          
+
         </p>
-
-        {/**
-        <header>
-          <Flash count={3}  bottom cascade>
-            <h1 className="nameHeader">{data.name}</h1>
-          </Flash>
-        </header>
-
-        <Flash delay={4000} duration={10000} forever={true}  cascade>
-          <h1 className="heading-background" style={{ color: '#292829' }}>
-            FULLFAT
-          </h1>
+        <Flash forever={true} duration={3000} delay={2100} cascade>
+          {' '}
+          <h1 className="heading-background">FULLFAT</h1>
         </Flash>
        
-
-       
-          <p className="header-title" style={{ color: '#cf9332' }}>
-            <Roll delay={1000}> {data.headerTagline[1]}</Roll>
-            <Roll delay={1500}> {data.headerTagline[2]}</Roll>
-            <Roll delay={2000}> {data.headerTagline[3]}</Roll>
-            <Roll delay={2500}> {data.headerTagline[4]}</Roll>
-          </p>
-       */}
-      </div>
+       </Col>
+       </Row>
+       <Row></Row>
+      </Container>
     );
   }
 }
