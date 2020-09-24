10579ae85d5b9a09b71f14e783b0b962f3c756ee
Niko Gham <ng@nickgillham.co.uk>
2020-09-24 21:01:16 +0100

    initial edits

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,34 @@
 import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
-import data from '../yourdata';
-import mainImg from '../assets/ffgMain.png';
+import { Container, Row, Col } from 'reactstrap';
 
+import data from '../yourdata';
 
 class About extends Component {
-    state = {  }
+  state = {};
 
-
-    render() { 
-
-        
-        return (
-        
-        <div className='about '>
-            <div className='about-content'>
-            <h1><Fade bottom cascade>About.</Fade></h1>
-            <Fade bottom>
-                    <p>{data.abouttext}</p>
-            </Fade>
-            </div>
-            {data.ShowAboutImage ? <img src={mainImg} alt='About' ></img> : null}
-            
-
-        </div>  );
-    }
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col>
+            <div className="about">
+              <div className="about-content">
+                <h1>
+                  <Fade left cascade>
+                    About.
+                  </Fade>
+                </h1>
+                <Fade bottom>
+                  <p>{data.abouttext}</p>
+                </Fade>
+              </div>
+            </div>{' '}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
- 
+
 export default About;
