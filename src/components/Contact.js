256378807e28ec17b51040af070c0ba545db238b
Niko Gham <ng@nickgillham.co.uk>
2020-10-03 15:19:09 +0100

    footer create

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,42 +1,60 @@
 import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
 import data from '../yourdata';
+import { Container, Row, Col, Form } from 'reactstrap';
+import Footer from './Footer';
 
 class Contact extends Component {
   state = {};
   render() {
     return (
-      <div>
-        <h1>
-          <Fade bottom cascade>
-            Contact us
-          </Fade>
-        </h1>
-        <Fade bottom>
-          <div className="contact-content">
+      <Container fluid>
+        <Row> {/* TOP ROW WITH HEADING  */ }
+          <Col>
             <h1>
-              Always use, <br></br>
-              <span className="amazing-color">Your Judo</span>
+              <Fade bottom cascade>
+                Contact us
+                <Fade left delay={500}>
+                  <hr />
+                </Fade>
+              </Fade>
             </h1>
-            <a href={`mailto:${data.contactEmail}`} className="email">
-              {data.contactEmail}
-            </a>
-            <ul>
-              {data.social.map((link, index) => (
-                <li key={index}>
-                  <a target="_blank" rel="noopener noreferrer" href={link.url}>
-                    {link.name} <i className="fab fa-instagram linkSocial"></i>
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </Fade>
-
-        <span className="footer">
-          Made by <a href="www.nickgillham.co.uk"> Nick @ Small Dog Designs</a>
-        </span>
-      </div>
+          </Col>
+        </Row>
+{/* MIDDLE ROW FOR CONTENT */ }
+        <Row>
+          <Col>
+            <Fade delay>
+              <iframe
+                width="600"
+                height="500"
+                src="https://maps.google.com/maps?q=full%20fat%20grappling,%20Radstock%20BA3%204BD&t=&z=13&ie=UTF8&iwloc=B&output=embed"
+                frameborder="0"
+                scrolling="no"
+                marginheight="0"
+                marginwidth="0"
+              ></iframe>
+            </Fade>
+          </Col>
+          <Col>
+          <Form>
+            <Form.Group>
+              
+            </Form.Group>
+          </Form>
+          </Col>
+        </Row>
+{/* BOTTOM ROW FOR FOOTER  */ }
+        <Row>
+          <Col>
+            <Fade bottom></Fade>
+          </Col>
+          <Col sm={'AUTO'}>
+            <Footer />
+          </Col>
+          {/*Why the fuck does AUTO work nicely but auto doesn;t? */}
+        </Row>
+      </Container>
     );
   }
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import data from '../yourdata'
+ const Footer = () => {
+    return (
+        <div>
+
+            <span className="footer-sdd">
+          Crafted by <a href="www.nickgillham.co.uk"> Nick @ Small Dog Designs</a>
+        </span>
+        </div>
+    )
+}
+
+
+export default Footer;
