1abb057b4a219cf478a39bc76e266c0343eae5ff
Niko Gham <ng@nickgillham.co.uk>
2020-10-10 08:24:18 +0100

    adult changes

--- src/components/Adult.js
+++ src/components/Adult.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React
+ from 'react';
+import Fade from 'react-reveal/Fade';
+import Footer from './Footer';
 import { Container, Row, Col } from 'reactstrap';
+import AdultCarousel from './subcomponents/AdultCarousel'
 
 /* Remember Niko, it's just fancy Javascript */ 
 
 
 const Adult = (props) => {
-
-  
   return (
     <Container>
-      <Row>
-        <Col>
-        <h1>Adults</h1>
-        </Col>
-      </Row>
-    </Container>
+    <Row>
+      <Col>
+        <div>
+          <Fade left>
+            <h1>Adult Athletes</h1>
+            <hr />
+          </Fade>
+        </div>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+      <AdultCarousel />
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+      <h2>Text </h2>
+      </Col>
+    </Row>
+    <Row>
+      <Footer />
+    </Row>
+  </Container>
   );
 };
 
