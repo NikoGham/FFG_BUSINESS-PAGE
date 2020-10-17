f150de8d1b8a30ae4e1926183a224d4e67780d7b
Niko Gham <ng@nickgillham.co.uk>
2020-10-17 23:11:00 +0100

    add table basics

--- src/components/Timetable.js
+++ src/components/Timetable.js
@@ -1,11 +1,67 @@
 import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade';
+import Footer from './Footer';
+import { Container, Row, Col, Table } from 'reactstrap';
 
 class Timetable extends Component {
   render() {
+
+    const headerStyle = {
+      margin: "3rem",
+     };
+
     return (
-      <div>
-        <h1>Timetable</h1>
-      </div>
+      <Container fluid>
+        <Row>
+          <Col>
+            <div>
+              <Fade left>
+                <h1 style={headerStyle}>Timetable <hr /></h1>
+                
+              </Fade>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+          <Table hover>
+      <thead>
+        <tr>
+          <th></th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+          </Col>
+        </Row>
+        <Row>
+          <Col >
+          <Footer />
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
