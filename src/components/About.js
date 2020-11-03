aa62cacfd332e81dff02a5d15c792f78ed2b1cd5
Niko Gham <ng@nickgillham.co.uk>
2020-11-03 14:00:35 +0000

    finally some nice styling

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,8 @@ import Fade from 'react-reveal/Fade';
 import { Container, Row, Col } from 'react-bootstrap';
 import Image from 'react-bootstrap/Image';
 import child from '../assets/CHILD_PHOTO.jpg';
+import child1 from '../assets/Adult.jpg';
+
 import data from '../yourdata';
 
 class About extends Component {
@@ -12,7 +14,12 @@ class About extends Component {
 
 	render() {
 		const honor = data.abouttextlist;
-		const honorList = honor.map((item, index) => <h4 key={index}>{item}<hr /></h4>);
+		const honorList = honor.map((item, index) => (
+			<h4 key={index}>
+				{item}
+				<hr />
+			</h4>
+		));
 
 		const headerStyle = {
 			margin: '3rem',
@@ -22,25 +29,20 @@ class About extends Component {
 
 		const styleList = {
 			color: 'var(--creamJapPalette)',
-			fontFamily: 'Nanum Myeongjo, serif', 
+			fontFamily: 'Nanum Myeongjo, serif',
 			fontSize: '200%',
 			textTransform: 'touppercase',
 			listStyle: 'none'
 		};
 
-		
 		return (
 			<Container className='childrenBG' fluid>
 				<Row>
 					<Col>
-							<div className='about'>
+						<div className='about'>
 							<Fade left cascade>
-									<h1 style={headerStyle}>
-										{' '}
-										{this.state.title} 
-									</h1>
-								</Fade>
-							
+								<h1 style={headerStyle}> {this.state.title}</h1>
+							</Fade>
 						</div>
 					</Col>
 				</Row>
@@ -56,21 +58,29 @@ class About extends Component {
 											<Image src={child} alt='Child Throw in Judo' thumbnail />
 										</div>
 									</Col>
-									<Col style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+									<Col style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
 										<ul style={styleList}> {honorList} </ul>
 									</Col>
 								</Row>
 								<Row>
-								<hr />
-									<p>{data.abouttext21}</p>
-									<hr />
-									<p>{data.abouttextQuote}</p>
-									<hr />
-									<p>{data.abouttext3}</p><hr />
-									<p>{data.abouttext31}</p><hr />
-									<p>{data.abouttext4}</p><hr />
+									<Col>
+										<p>{data.abouttext21}</p>
+										<hr />
+
+										<p>{data.abouttextQuote}</p>
+
+										<Image src={child} alt='Child Throw in Judo' thumbnail />
+
+										<hr />
+										<p>{data.abouttext3}</p>
+										<hr />
+										<p>{data.abouttext31}</p>
+										<hr />
+										<p>{data.abouttext4}</p>
+										<hr />
+									</Col>
 								</Row>
-							</Fade> 
+							</Fade>
 						</div>
 					</Col>
 					<Col md={2} />
