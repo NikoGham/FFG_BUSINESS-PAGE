8291ead9a1a9baf9137896e84668f4e4f0e66ccd
Niko Gham <ng@nickgillham.co.uk>
2020-11-15 16:39:59 +0000

    style edit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,62 +1,95 @@
 import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
-import { Container, Row, Col} from 'react-bootstrap';
-import Footer from './Footer'
-import ContactForm from "./subcomponents/ContactForm"
+import { Container, Row, Col } from 'react-bootstrap';
+import Footer from './Footer';
+import ContactForm from './subcomponents/ContactForm';
 
 class Contact extends Component {
-  state = {};
-  render() {
-    return (
-      <Container className='childrenBG contact' fluid>
-
-        <Row>
-          {/* TOP ROW WITH HEADING  */}
-          <Col>
-            <h1>
-              <Fade bottom cascade>
-                Contact us
-                <Fade left delay={500}>
-                  <hr />
-                </Fade>
-              </Fade>
-            </h1>
-          </Col>
-        </Row>
-        {/* MIDDLE ROW FOR CONTENT */}
-        <Row>
-          <Col md={1} />
-          <Col md={5}>
-            <Fade delay>
-              <iframe
-                width="600"
-                height="500"
-                src="https://maps.google.com/maps?q=full%20fat%20grappling,%20Radstock%20BA3%204BD&t=&z=13&ie=UTF8&iwloc=B&output=embed"
-                frameborder="0"
-                scrolling="no"
-                marginheight="0"
-                marginwidth="0"
-                title="Location"
-              ></iframe>
-            </Fade>
-          </Col>
-                  {/* FORM COLUMN */}
-        
-          <Col md={5}>
-            <Fade delay={500}>
-            <ContactForm></ContactForm>
-            </Fade>
-          
-
-          </Col>
-        </Row>
-        {/* BOTTOM ROW FOR FOOTER  */}
-        <Row style={{ display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-					<Footer />
+	state = {};
+	render() {
+		return (
+			<Container className='childrenBG contact' fluid>
+				<Row>
+					{/* TOP ROW WITH HEADING  */}
+					<Col>
+						<h1>
+							<Fade bottom cascade>
+								Contact us
+								<Fade left delay={500}>
+								</Fade>
+							</Fade>
+						</h1>
+					</Col>
 				</Row>
-      </Container>
-    );
-  }
+				{/* MIDDLE ROW FOR CONTENT */}
+				<Row>
+					<Col md={1} />
+					<Col md={5}>
+						<Fade delay>
+							<iframe
+								width='600'
+								height='500'
+								src='https://maps.google.com/maps?q=full%20fat%20grappling,%20Radstock%20BA3%204BD&t=&z=13&ie=UTF8&iwloc=B&output=embed'
+								frameborder='0'
+								scrolling='no'
+								marginheight='0'
+								marginwidth='0'
+								title='Location'
+							/>
+						</Fade>
+					</Col>
+					{/* FORM COLUMN */}
+
+					<Col md={5}>
+						<Fade delay={500}>
+							<ContactForm />
+						</Fade>
+					</Col>
+					<Col md={1} />
+				</Row>
+				{/*FOOTER SECTION */}
+				<Row style={FooterContainer}>
+					<Row style={styleDiv}>
+						<Footer />
+					</Row>
+					<Row style={styleDiv2}>
+						<p> Unit 4, 24 Norbury Road, Cardiff, CF5 3AU | 08420934824 | info@fullfatgrappling.com</p>
+					</Row>
+				</Row>
+			</Container>
+		);
+	}
 }
 
+const styleDiv = {
+	backgroundColor: 'var(--goldGrappFont)',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+	height: '8vh',
+	width: '100%'
+};
+
+const styleDiv2 = {
+	backgroundColor: 'var(--optimalOnBlack)',
+	display: 'flex',
+	alignContent: 'center',
+	alignItems: 'center',
+
+	justifyContent: 'center',
+	height: '7vh',
+	width: '100%',
+	color: '#000',
+	fontFamily: 'Montserrat'
+};
+
+const FooterContainer = {
+	backgroundColor: 'var(--caramelFont)',
+	position: 'relative',
+	width: 'auto',
+	bottom: '0px',
+	display: 'flex',
+	justifyContent: 'center',
+	marginTop: '1rem'
+};
 export default Contact;
