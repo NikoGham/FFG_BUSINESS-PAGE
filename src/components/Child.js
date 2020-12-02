1a29853d783824c75b54cc316c45a019350019a4
Niko Gham <ng@nickgillham.co.uk>
2020-12-02 19:57:46 +0000

    god damn footer fix

--- src/components/Child.js
+++ src/components/Child.js
@@ -102,51 +102,29 @@ class Child extends Component {
 					</Col>
 					<Col md={2} />
 				</Row>
-				{/*FOOTER SECTION */}
-				<Row style={FooterContainer}>
-					<Row style={styleDiv}>
-						<Footer />
-					</Row>
-					<Row style={styleDiv2}>
-						<p>
-							{' '}
-							Full Fat Grappling, Pratten's Charlton La, Radstock BA3 4BD, United Kingdom| 07877 816 749 |
-							info@fullfatgrappling.com
-						</p>{' '}
-					</Row>
+ 			{/*FOOTER SECTION */}
+
+			 <Row style={styleDiv2}>
+					<Footer />
 				</Row>
 			</Container>
 		);
 	}
 }
 
-const styleDiv = {
-	backgroundColor: 'var(--goldGrappFont)',
-	display: 'flex',
-	alignItems: 'center',
-	justifyContent: 'center',
-	height: '8vh',
-	width: '100%'
-};
-
 const styleDiv2 = {
 	backgroundColor: 'var(--caramelFont)',
 	display: 'flex',
-	alignContent: 'center',
-	justifyContent: 'center',
-	height: '7vh',
-	width: '100%',
-	color: '#000',
-	fontFamily: 'Montserrat'
-};
+	flexDirection: 'row',
 
-const FooterContainer = {
-	backgroundColor: 'var(--caramelFont)',
-	position: 'static',
-	width: 'auto',
+	justifyContent: 'center',
+	height: '15vh',
+	width: '100vw',
+	color: '#000',
+	fontFamily: 'Montserrat',
+	position: 'relative',
 	bottom: '0px',
-	display: 'flex',
-	justifyContent: 'center'
+	marginTop: '10vh'
 };
 
 const carouselContainer = {
