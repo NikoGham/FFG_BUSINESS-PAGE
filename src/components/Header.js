e0dc224e2d6216ea9bf661f8b4fbbbf7aa0db4f2
Niko Gham <ng@nickgillham.co.uk>
2020-12-22 19:20:43 +0000

    HEADER REFACTOR AND STYLE EDIT

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,23 @@
-import React, { Component } from 'react';
-import Fade from 'react-reveal/Fade';
-import Flash from 'react-reveal/Flash';
-import data from '../yourdata';
+import React, { Component } from 'react'
+
 import Particles from './subcomponents/Particles'
-import { Container, Row, Col } from 'react-bootstrap'
 
 class Header extends Component {
-	state = {};
+	state = {}
 
 	render() {
 		return (
-			<Container className='homeBG' fluid>
+			<div className='homeBG'>
 				<Particles />
-				<Row>
-					<Col>
-						<p className='header-title'>
-							<Fade duration={5000} delay={2000}>
-								<span className='nameHeader'>{data.headerTagline[0]}</span>
-							</Fade>
-						</p>
-					</Col>
-				</Row>
 
-				<Row>
-					<Col md={12}>
-						<Flash  delay={2100} >
-							<h1 className='heading-background'>FULL FAT </h1>
-						</Flash>
-					</Col>
-				</Row>
-				<Row>
-					<Col md={12}>
-						<Flash  delay={2100} >
-							<h1 id='grappleStyle'>
-								GRAPPLING
-							</h1>
-						</Flash>
-					</Col>
-				</Row>
-			</Container>
-		);
+				<p id='header-title'>Matthew Hucker's</p>
+
+				<h1 id='fullFatTitle'>FULL FAT </h1>
+
+				<h1 id='grappleStyle'>GRAPPLING</h1>
+			</div>
+		)
 	}
 }
 
-export default Header;
+export default Header
