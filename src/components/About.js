a4c80a3c128ff9176338ffaece18c53afbb3a07b
Niko Gham <ng@nickgillham.co.uk>
2020-12-23 19:39:14 +0000

    About Edits

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,8 @@ import { Row } from 'react-bootstrap'
 import Image from 'react-bootstrap/Image'
 import child from '../assets/CHILD_PHOTO.jpg'
 import child1 from '../assets/Adult.jpg'
+import child2 from '../assets/bvsg.jpg'
+
 import headImage from '../assets/17.jpg'
 import Footer from './Footer'
 
@@ -35,7 +37,6 @@ class About extends Component {
 						<hr />
 					</Fade>
 				</div>
-
 				<div className='about-content'>
 					<div id='headContainer'>
 						<div id='listStyle'>
@@ -96,7 +97,8 @@ class About extends Component {
 						<Fade right slower>
 							<div id='headParaContainer'>
 								<p id='headPara'>
-									Welcome to FULL FAT GRAPPLING. <br /> The home of great ambition and high expectation.<br />
+									Welcome to FULL FAT GRAPPLING. <br /> The home of great ambition and high
+									expectation.<br />
 									<br /> Our focus is to benefit anyone in any grappling sport, specialising in the
 									olympic sport of JUDO.
 								</p>
@@ -106,39 +108,32 @@ class About extends Component {
 
 					<Fade left slow>
 						<div className='imageContainerAbout'>
-							<div>
-								<Image className='imageAbout' src={child} alt='Child Throw in Judo' />
-							</div>
-							<div>
-								<p>{data.abouttext21}</p>
-							</div>
+							<Image className='imageAbout' src={child} alt='Child Throw in Judo' />
+							<p>
+								{data.abouttext2} <br /> <br />
+								{data.abouttext21}
+							</p>
 						</div>
 					</Fade>
 					<Fade left slow>
 						<div className='imageContainerAbout'>
-							<div>
-								<Image className='imageAbout' src={child} alt='Child Throw in Judo' />
-							</div>
-							<div>
-								<p>{data.abouttextQuote}</p>
-							</div>
+							<p>{data.abouttextQuote}</p>
+
+							<Image className='imageAbout' src={child2} alt='Child Throw in Judo' />
 						</div>
 					</Fade>
 
 					<Fade left slow>
 						<div className='imageContainerAbout'>
-							<div>
-								<Image className='imageAbout' src={child1} alt='Child Throw in Judo' />
-							</div>
-							<div>
-								<p>{data.abouttext3}</p>
-							</div>
+							<Image className='imageAbout' src={child1} alt='Child Throw in Judo' />
+							<p>{data.abouttext3}</p>
 						</div>
 					</Fade>
 
 					<Fade left slow>
-						<p>{data.abouttext4}</p>
-						<hr />
+						<div className='imageContainerAbout'>
+							<p id='bottomTextAbout'>{data.abouttext4}</p>
+						</div>
 					</Fade>
 				</div>
 
