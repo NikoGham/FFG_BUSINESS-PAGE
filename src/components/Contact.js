8792b2ff36f6b65559663e80cb5de20d4ba0bf3b
Niko Gham <ng@nickgillham.co.uk>
2021-02-20 11:26:58 +0000

    component fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,64 +4,49 @@ import { Container, Row, Col } from 'react-bootstrap'
 import Footer from './Footer'
 import ContactForm from './subcomponents/ContactForm'
 
-class Child extends Component {
-	render() {
-		return (
-			<Container className='childrenBG' fluid>
-				{/*CONTENT SECTION */}
-				<Row>
-					<Col>
-						<div>
-							<Fade left cascade>
-								<h1 id='memTitle'>
-									contact
-								</h1>
-							</Fade>
-						</div>
-					</Col>
-				</Row>
-				<Row>
-					<Col sm={1} />
-					<Col className='childrenText' sm={5}>
-						<Fade left slow>
-							<iframe
-								src='https://maps.google.com/maps?q=full%20fat%20grappling,%20Radstock%20BA3%204BD&t=&z=13&ie=UTF8&iwloc=B&output=embed'
-								frameBorder='100'
-								scrolling='no'
-								marginHeight='0'
-								marginWidth='0'
-								title='Location'
-							/>
-						</Fade>
-					</Col>
-					<Col sm={5}>
-						<Fade left slow>
-							<ContactForm />
-						</Fade>
-					</Col>
-					<Col sm={1} style={style3} />
-				</Row>
-				{/*FOOTER SECTION */}
-				<Row style={styleDiv2}>
-
-					<Footer />
-				</Row>
-			</Container>
-		)
-	}
+class Contact extends Component {
+ render() {
+  return (
+   <Container fluid>
+    {/*CONTENT SECTION */}
+    <Row>
+     <Col>
+      <div>
+       <Fade left cascade>
+        <h1 id="contactTitle">contact</h1>
+       </Fade>
+      </div>
+     </Col>
+    </Row>
+    <Row>
+     <Col />
+     <Col>
+      <Fade left slow>
+       <iframe
+        src='https://maps.google.com/maps?q=full%20fat%20grappling,%20Radstock%20BA3%204BD&t=&z=13&ie=UTF8&iwloc=B&output=embed'
+        frameBorder='100'
+        scrolling='no'
+        marginHeight='0'
+        marginWidth='0'
+        title='Location'
+       />
+      </Fade>
+     </Col>
+     <Col />
+    </Row>
+    <Row className="contactFormRow" >
+      <Fade left slow>
+       <ContactForm />
+      </Fade>
+    </Row>
+    {/*FOOTER SECTION */}
+    <Row className="footerDrop">
+     <Footer  />
+    </Row>
+   </Container>
+  )
+ }
 }
 
-const styleDiv2 = {
-	backgroundColor: 'var(--caramelFont)',
-	flexDirection: 'row',
-	justifyContent: 'center',
-	position: 'absolute',
-	bottom: '0px',
-	width: '100vw',
-	marginTop: '20px'
-}
 
-const style3 = {
-	marginTop: '200px'
-}
-export default Child
+export default Contact
--- src/components/subcomponents/ContactForm.js
+++ src/components/subcomponents/ContactForm.js
@@ -1,28 +1,29 @@
-import React from "react";
-
+import React from 'react'
 import { MDBBtn, MDBIcon, MDBInput } from 'mdbreact';
 
+
 const ContactForm = () => {
-return (
 
-      <form name="contact" method="post">
-        <input type="hidden" name="form-name" value="contact" />
-        <div className="grey-text subFontText">
-          <MDBInput label="Your name" name="name"  group type="text" validate error="wrong"
-            success="right" />
-          <MDBInput required label="Your email" name="email" group type="email" validate error="wrong"
-            success="right" />
-          <MDBInput label="Subject" name="subject" group type="text"  validate error="wrong" success="right" />
-          <MDBInput type="textarea" rows="2" label="Your message" name="message" />
-        </div>
-        <div  className="text-center ">
-          <MDBBtn type="submit" outline className="subTextFont" color="warning">
-            Send
-            <MDBIcon far icon="paper-plane" className="ml-1" />
-          </MDBBtn>
-        </div>
-      </form>
-);
-};
+ return (
 
-export default ContactForm;
+  <form name="contact" method="post" id="contactForm">
+  <input type="hidden" name="form-name" value="contact" />
+  <div className="grey-text subFontText">
+    <MDBInput label="Your name" name="name"  group type="text" validate error="wrong"
+      success="right" />
+    <MDBInput required label="Your email" name="email" group type="email" validate error="wrong"
+      success="right" />
+    <MDBInput label="Subject" name="subject" group type="text"  validate error="wrong" success="right" />
+    <MDBInput type="textarea" rows="2" label="Your message" name="message"  />
+  </div>
+  <div  className="text-center ">
+    <MDBBtn type="submit" outline className="subTextFont" color="warning">
+      Send
+      <MDBIcon far icon="paper-plane" className="ml-1" />
+    </MDBBtn>
+  </div>
+</form>
+ )
+}
+
+export default ContactForm
